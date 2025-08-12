679f76103868be53de58e104b3bc4bf979f7a270
luxeshadow <agodaclarisse@gmail.com>
2025-08-12 08:24:22 +0000

    first commit

---
+++ src/components/layouts/footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer class="bg-white border-t mt-12 py-8">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="md:flex md:items-center md:justify-between">
+        <div class="flex justify-center md:order-2 space-x-6">
+          <a href="#" class="text-gray-400 hover:text-gray-500">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <a href="#" class="text-gray-400 hover:text-gray-500">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a href="#" class="text-gray-400 hover:text-gray-500">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+        </div>
+        <div class="mt-8 md:mt-0 md:order-1">
+          <p class="text-center text-sm text-gray-500">
+            &copy; CEET Feedback. Tous droits réservés.
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
