a8b2ba42594b52ad5f753c69de7e610603626f45
luxeshadow <luxe9152@gmail.com>
2025-08-28 08:12:37 +0000

    fix:change ngrok link

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,3 +1,3 @@
 // constants.ts
 // export const API_BASE_URL = 'http://127.0.0.1:8000/api/v1/';
-export const API_BASE_URL = 'https://4e7544645c2a.ngrok-free.app/api/v1';
+export const API_BASE_URL = ' https://20608b18f4f5.ngrok-free.app/api/v1';
