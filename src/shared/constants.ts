6f43a7225280cc7f370741c85ffb7bb21867f5d7
luxeshadow <luxe9152@gmail.com>
2025-09-02 15:32:45 +0000

    fix: create feedback add ngrok link

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,3 +1,3 @@
 // constants.ts
-export const API_BASE_URL = 'http://127.0.0.1:8000/api/v1/';
-// export const API_BASE_URL = 'https://dc46b588434f.ngrok-free.app/api/v1';
+// export const API_BASE_URL = 'http://127.0.0.1:8000/api/v1/';
+export const API_BASE_URL = 'https://7d3275a10a1f.ngrok-free.app/api/v1';
